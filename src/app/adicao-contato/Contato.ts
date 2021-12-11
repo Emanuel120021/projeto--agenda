2646de95de2641685039eee11c334ae65bc85fc0
Emanuel120021 <emanuel.lazaro.52@gmail.com>
2021-12-11 13:28:49 -0300

    adicao do campo aniversário

--- src/app/adicao-contato/Contato.ts
+++ src/app/adicao-contato/Contato.ts
@@ -2,11 +2,13 @@ export class Contato {
   private __nome!: string;
   private __telefone!: string;
   private __email!: string;
+  private __aniversario!: string;
 
   constructor() {
     this.nome = '';
     this.telefone = '';
     this.email = '';
+    this.aniversario = '';
   }
 
   set nome(nome: string) {
@@ -18,6 +20,9 @@ export class Contato {
   set email(email: string) {
     this.__email = this.email;
   }
+  set aniversario(aniversario: string) {
+    this.__aniversario = this.aniversario;
+  }
 
   get nome(): string {
     return this.__nome;
@@ -27,5 +32,10 @@ export class Contato {
   }
   get email(): string {
     return this.__email;
+  }
+  get aniversario(): string {
+    // let niver = this.__aniversario;
+    // return `${niver[2]}/${niver[1]}/${niver[0]}`;
+    return this.__aniversario;
   }
 }
